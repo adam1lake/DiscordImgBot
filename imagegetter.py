ffbad8c757b4c9963341dbb6eb64b1691c84ff65
adam1lake <54553845+adam1lake@users.noreply.github.com>
2022-09-21 20:30:52 +0100

    Revert "Updated for latest discord and selenium changes."
    This reverts commit 89362a63205de51362b63847813964193b24b0e5.

---
+++ imagegetter.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from random import randint
+import shutil
+import urllib.request
+from selenium.webdriver.chrome.options import Options
+
+
+class ImageGetter:
+    # Driver set up
+    def __init__(self):
+        options = Options()
+        # Comment out the line below to disable headless Chrome for debugging
+        options.add_argument("--headless")
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.implicitly_wait(2)
+
+    # Get image using query string. By default, random is off and the first result is returned
+    def get_image(self, query, random=False):
+        self.driver.get(f"https://www.google.com/search?tbm=isch&q={query}")
+        # Sets the index of the image on the page
+        if random:
+            result_id = randint(1, 50)
+        else:
+            result_id = 1
+
+        # JavaScript code to get the image using the index and click on it
+        self.driver.execute_script("""
+        try {
+            let element = document.evaluate('(//div[@id="islmp"]//img)[%s]', document, XPathResult.singleNodeValue).iterateNext();
+            element.click();
+        } catch {
+            console.log("Failed to click on element.");
+        } 
+        """ % result_id)
+
+        # TODO: get a higher resolution image, as the current source gives an image of relatively low resolution
+        # Finds multiple images
+        elements = self.driver.find_elements_by_xpath(
+            "(//img[@class='n3VNCb' and @jsname='HiaYvf' and 'load:XAeZkd;'])")
+
+        url = ""
+        print(len(elements))
+        # Selects the correct image that has the desired source
+        if len(elements) == 0:
+            # This occurs when no results are found for the search query
+            return "NoResults"
+        elif len(elements) == 1 or len(elements) == 2:
+            element_id = 0
+        elif len(elements) == 3:
+            element_id = 1
+        else:
+            return "Error"
+
+        # Downloads the image
+        img = urllib.request.urlopen(elements[element_id].get_attribute("src"))
+
+        # Saves the image to the root directory
+        with open("current_image.png", "wb") as f:
+            shutil.copyfileobj(img, f)
+            print("Image saved.")
+            return True
